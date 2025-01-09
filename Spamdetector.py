830c01b25113c36f61d3457322f374a0186a8bed
Samar raj <147753477+sam7836@users.noreply.github.com>
2025-01-09 12:54:27 +0530

    Update Spamdetector.py

--- Spamdetector.py
+++ Spamdetector.py
@@ -4,8 +4,7 @@ import pickle
 import string
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-import nltk
-nltk.download('punkt')
+
 
 
 # Download required NLTK data only once
